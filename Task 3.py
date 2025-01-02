96d1697fea4c1ffe351982a2a0e1a76f6277cb9e
Kirill <Kirill0098@gmail.com>
2025-01-02 22:38:36 +0500

    completed task 3 new

--- Task 3.py
+++ Task 3.py
@@ -40,8 +40,18 @@ txt3 = Line('3.txt')
 txt3.write_file()
 
 # Решение Задания 3
-task_3 = Line()
-task_3.new_file(txt1.file_dict, txt2.file_dict, txt3.file_dict)
+#task_3 = Line()
+#task_3.new_file(txt1.file_dict, txt2.file_dict, txt3.file_dict)
 
+### Сортировка от П понял, но задание просто не так понял немного
+def merge_files(file_names):
+    sorted_file_data = sorted(((name, len(open(name, encoding='utf-8').readlines())) for name in file_names), key=lambda x: x[1])
 
+    with open('result.txt', 'w', encoding='utf-8') as result_file:
+        for file_name, line_count in sorted_file_data:
+            file_content = open(file_name, encoding='utf-8').read()
+            result_file.write(f'Имя файла: {file_name}\n')
+            result_file.write(f'Количество строк: {line_count}\n')
+            result_file.write(file_content + '\n\n')
 
+merge_files(['1.txt', '2.txt', '3.txt'])
